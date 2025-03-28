cea7345bb4ad4dd087e728b9bf343d69ff21d605
tlpss <thomas17.lips@gmail.com>
2025-03-28 13:17:45 +0100

    build lerobot dataset recorder  from example (obs,action) pair

--- robot_imitation_glue/dataset_recorder.py
+++ robot_imitation_glue/dataset_recorder.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 import torch
 import numpy as np 
-from robot_imitation_glue.base import BaseEnv, BaseDatasetRecorder
+from robot_imitation_glue.base import BaseDatasetRecorder
 
 
 class DummyDatasetRecorder(BaseDatasetRecorder):
@@ -42,7 +42,7 @@ class LeRobotDatasetRecorder(BaseDatasetRecorder):
         },
     }
 
-    def __init__(self, env: BaseEnv, root_dataset_dir: Path, dataset_name: str, fps: int, use_videos=True):
+    def __init__(self, example_obs_dict: dict, example_action: np.array, root_dataset_dir: Path, dataset_name: str, fps: int, use_videos=True):
         from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 
         self.root_dataset_dir = root_dataset_dir
@@ -53,54 +53,35 @@ class LeRobotDatasetRecorder(BaseDatasetRecorder):
         self.key_mapping_dict = {}
 
 
-        # TODO: either add mapping to robot env OR infer from an observation DICT.
+        self.image_keys = []
+        self.state_keys = []
 
+        # create features  using the example dict. assume all numpy arrays. 0D is a scalar, 1D with size 1 is a scalar, 1D with size > 1 is a vector, 3D is an image.
+        features = self.DEFAULT_FEATURES.copy()
+        for key, value in example_obs_dict.items():
+            shape = value.shape
+            if len(shape) == 0:
+                features[key] = {"dtype": "float32", "shape": (1,), "names": None}
+            elif len(shape) == 1:
+                features[key] = {"dtype": "float32", "shape": shape, "names": None}
+            elif len(shape) == 3:
+                if use_videos:
+                    features[key] = {"dtype": "video", "names": ["channel", "height", "width"], "shape": shape}
+                else:
+                    features[key] = {"dtype": "image", "shape": shape, "names": None}
+            else:
+                raise ValueError(f"Unsupported shape for feature {key}: {shape}")
 
-        # features = self.DEFAULT_FEATURES.copy()
-        # # add images to features
-
-        # # uses the lerobot convention to map to 'observation.image' keys
-        # # and stores as video.
-
-        # assert isinstance(env.observation_space, gymnasium.spaces.Dict), "Observation space should be a dict"
-        # self.image_keys = [key for key in env.observation_space.spaces.keys() if "image" in key]
-        # num_cameras = len(self.image_keys)
-        # for key in self.image_keys:
-        #     shape = env.observation_space.spaces[key].shape
-
-        #     if not key.startswith("observation.images"):
-        #         lerobot_key = f"observation.images.{key}"
-        #         self.key_mapping_dict[key] = lerobot_key
-
-        #     lerobot_key = self.key_mapping_dict.get(key, key)
-        #     if "/" in lerobot_key:
-        #         self.key_mapping_dict[key] = lerobot_key.replace("/", "_")
-        #     lerobot_key = self.key_mapping_dict[key]
-        #     if use_videos:
-        #         features[lerobot_key] = {"dtype": "video", "names": ["channel", "height", "width"], "shape": shape}
-        #     else:
-        #         features[lerobot_key] = {"dtype": "image", "shape": shape, "names": None}
-
-        # # state observations
-        # self.state_keys = [key for key in env.observation_space.spaces.keys() if key not in self.image_keys]
-        # for key in self.state_keys:
-        #     shape = env.observation_space.spaces[key].shape
-        #     features[key] = {"dtype": "float32", "shape": shape, "names": None}
-
-        # # add single 'state' observation that concatenates all state observations
-        # features["observation.state"] = {
-        #     "dtype": "float32",
-        #     "shape": (sum([env.observation_space.spaces[key].shape[0] for key in self.state_keys]),),
-        #     "names": None,
-        # }
-        # # add action to features
-        # features["action"] = {"dtype": "float32", "shape": env.action_space.shape, "names": None}
-
-        #print(f"Features: {features}")
-        # create the dataset
+            if  len(shape) == 3:
+                self.image_keys.append(key)
+            else:
+                self.state_keys.append(key)
+        
+        # add action to features
+        features["action"] = {"dtype": "float32", "shape": example_action.shape, "names": None}
+        print(f"Features: {features}")
 
         #TODO: if dataset exists, load it to extend it.
-
         
         self.lerobot_dataset = LeRobotDataset.create(
             repo_id=dataset_name,
@@ -116,14 +97,12 @@ class LeRobotDatasetRecorder(BaseDatasetRecorder):
         pass
 
     def record_step(self, obs, action):
-        timestamp = self.lerobot_dataset.episode_buffer["size"] / self.fps
-
         frame = {
             "action": torch.from_numpy(action),
-            "next.reward": torch.tensor(0.0),
-            "next.success": torch.tensor(False),
-            "seed": torch.tensor(0),  # TODO: store the seed
-            "timestamp": timestamp,
+            "next.reward": torch.tensor([0.0]),
+            "next.success": torch.tensor([False]),
+            "seed": torch.tensor([0]),  # TODO: store the seed
+            "task":""
         }
         for key in self.image_keys:
             lerobot_key = self.key_mapping_dict.get(key, key)
@@ -131,21 +110,56 @@ class LeRobotDatasetRecorder(BaseDatasetRecorder):
 
         for key in self.state_keys:
             frame[key] = torch.from_numpy(obs[key])
-
-        # concatenate all 'state' observations into a single tensor
-        state = torch.cat([frame[key].flatten() for key in self.state_keys])
-        frame["observation.state"] = state
-
         self.lerobot_dataset.add_frame(frame)
 
     def save_episode(self):
-        self.lerobot_dataset.save_episode(task="")
+        self.lerobot_dataset.save_episode()
         self._n_recorded_episodes += 1
 
     def finish_recording(self):
-        # computing statistics
-        self.lerobot_dataset.consolidate()
+        pass
 
     @property
     def n_recorded_episodes(self):
         return self._n_recorded_episodes
+    
+
+if __name__ == "__main__":
+
+    example_obs = {
+        "robot_pose": np.array([0.1, 0.2, 0.3], dtype=np.float32),
+        "image": np.random.rand(3, 64, 64).astype(np.float32),
+        "image2": np.random.rand(3, 128, 64).astype(np.float32),
+        "gripper_state": np.array([0.1],dtype=np.float32),
+
+    }
+
+    example_action = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7],dtype=np.float32)
+
+    import os 
+    # remove entire dataset directory
+    os.system("rm -rf datasets")
+    dataset_recorder = LeRobotDatasetRecorder(
+        example_obs_dict=example_obs,
+        example_action=example_action,
+        root_dataset_dir=Path("datasets"),
+        dataset_name="test_dataset",
+        fps=30,
+        use_videos=False,
+    )
+
+    for j in range(3):
+        dataset_recorder.start_episode()
+        for i in range(10-j):
+            dataset_recorder.record_step(example_obs, example_action)
+        dataset_recorder.save_episode()
+
+    dataset_recorder.finish_recording()
+    print(f"Recorded {dataset_recorder.n_recorded_episodes} episodes.")
+
+    from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
+    dataset = LeRobotDataset(repo_id="test_dataset", root=Path("datasets"),episodes=[0,1])
+    print(f"Loaded {len(dataset)} steps.")
+    print(dataset.episode_data_index)
+
+
