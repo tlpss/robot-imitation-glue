9ccb0f7864409deddc766fe9657e0a72c35f5eb9
tlpss <thomas17.lips@gmail.com>
2025-04-01 17:54:53 +0200

    make spacemouse parameters configurable

--- robot_imitation_glue/spacemouse_agent.py
+++ robot_imitation_glue/spacemouse_agent.py
@@ -18,9 +18,20 @@ class SpaceMouseAgent(BaseAgent):
 
     ACTION_SPEC = None
 
-    def __init__(self, buffer_size=10):  # Add buffer_size as a parameter
-        self.state_buffer = deque(maxlen=buffer_size)  # Use deque as a rolling buffer
+    def __init__(self, deadzone=0.1, translation_scale=0.02, rotation_scale=0.02, gripper_step_size=0.01):
+        """
+        Args:
+            deadzone: if input of spacemouse is below this value, it is set to 0. makes it easier to teleop single axis.
+            translation_scale: scale for translation, this is the real-world displacement per spacemouse action in meters.
+            rotation_scale: scale for rotation, this is the real-world rotation per spacemouse action in radians.
+            gripper_step_size: step size for gripper, this is the real-world displacement per spacemouse action in meters.
+        """
+        self.state_buffer = deque(maxlen=10)  # Use deque as a rolling buffer
         self.running = True
+        self.deadzone = deadzone
+        self.translation_scale = translation_scale
+        self.rotation_scale = rotation_scale
+        self.gripper_step_size = gripper_step_size
 
         # separate thread needed for continuous reading of SpaceMouse
         # cf. https://github.com/wuphilipp/gello_software/blob/main/gello/agents/spacemouse_agent.py
@@ -61,28 +72,23 @@ class SpaceMouseAgent(BaseAgent):
 
         pos = [state.x, state.y, state.z]
 
-        # TODO: make these configurable
-        deadzone_value = 0.1
-        rescale_pos = 0.02
-        rescale_rot = 0.02
-
         for i in range(3):
-            if abs(pos[i]) < deadzone_value:
+            if abs(pos[i]) < self.deadzone:
                 pos[i] = 0
             else:
-                pos[i] *= rescale_pos
+                pos[i] *= self.translation_scale
 
         for i in range(3):
-            if abs(rot[i]) < deadzone_value:
+            if abs(rot[i]) < self.deadzone:
                 rot[i] = 0
             else:
-                rot[i] *= rescale_rot
+                rot[i] *= self.rotation_scale
 
         gripper_action = 0
         if state.buttons[1]:
-            gripper_action = -0.01
+            gripper_action = -self.gripper_step_size
         elif state.buttons[0]:
-            gripper_action = 0.01
+            gripper_action = self.gripper_step_size
 
         return [*pos, *rot, gripper_action]
 
@@ -94,7 +100,7 @@ class SpaceMouseAgent(BaseAgent):
 
 if __name__ == "__main__":
     try:
-        agent = SpaceMouseAgent(buffer_size=5)  # set buffer size.
+        agent = SpaceMouseAgent()  # set buffer size.
         while True:
             action = agent.get_action()
             print(f"Action: {action}")
